a3475ccf85927dce7754dd3e09da98261104e455
Sara <sara@Saras-MBP.home.local>
2025-03-18 13:12:59 -0700

    Feature branch for Voting Cycle History
    [#260]

---
+++ src/app/governance/components/VotingHistory.tsx
@@ -0,0 +1,24 @@
+// TODO import data from a VotingHistory context; see #261
+
+export function VotingHistoryMobile() {
+  return (
+    <div>
+      <p>Ended on #TODO add distribution date</p>
+      {/* TODO add mobile components */}
+    </div>
+  );
+}
+
+export function VotingHistoryDesktop() {
+  return (
+    <div>
+      <h2>Past cycle</h2>
+      <p>
+        These are the results of the previous voting cycle #TODO - add voting
+        cycle number.
+      </p>
+      <small>Distribution date: #TODO - add distribution date</small>
+      {/* TODO add desktop components */}
+    </div>
+  );
+}
